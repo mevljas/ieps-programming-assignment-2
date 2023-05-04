4014fa844d33252efa7f05648adfaeccb45d5427
Sebastjan Mevlja <38669826+mevljas@users.noreply.github.com>
2023-05-04 15:34:10 +0200

    improve tests

--- implementation-extraction/road_runner/tests/integrations_tests.py
+++ implementation-extraction/road_runner/tests/integrations_tests.py
@@ -11,31 +11,24 @@ class MyTestCase(unittest.TestCase):
     def test_simple_case(self):
         html_1 = read_file("../../../input-extraction/tests/simple_case/page1.html", "utf-8")
         html_2 = read_file("../../../input-extraction/tests/simple_case/page2.html", "utf-8")
+        expected_result = read_file("../../../input-extraction/tests/simple_case/result.html", "utf-8")
         temp_stdout = StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             road_runner(first_html=html_1, second_html=html_2)
         output = temp_stdout.getvalue().strip()
-        self.assertEqual(output, """<html>
-books of:
-<b>
-#PCDATA
-</b>
-( img )?
-<ul>
-( <li><i>title:</i>#PCDATA</li> )+
-</ul>
-</html>""")
+        self.assertEqual(output, expected_result)
 
     @unittest.SkipTest
     def test_complex_case(self):
         html_1 = read_file("../../../input-extraction/tests/complex_case/page1.html", "utf-8")
         html_2 = read_file("../../../input-extraction/tests/complex_case/page2.html", "utf-8")
+        expected_result = read_file("../../../input-extraction/tests/complex_case/result.html", "utf-8")
         temp_stdout = StringIO()
         with contextlib.redirect_stdout(temp_stdout):
             road_runner(first_html=html_1, second_html=html_2)
         output = temp_stdout.getvalue().strip()
         print(output)
-        self.assertEqual(output, """""")
+        self.assertEqual(output, expected_result)
 
 
 if __name__ == '__main__':
